cf81842c46b3217c6b8950bee31a247fe793d722
enslaved46 <khadkaxashish@gmail.com>
2018-03-26 23:49:53 -0500

    ryu pic

---
+++ ryu.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+:VBSDynamicBuild
+SET TempVBSFile=%temp%\~tmpSendKeysTemp.vbs
+IF EXIST "%TempVBSFile%" DEL /F /Q "%TempVBSFile%"
+ECHO Set WshShell = WScript.CreateObject("WScript.Shell") >>"%TempVBSFile%"
+ECHO Wscript.Sleep 900                                    >>"%TempVBSFile%"
+ECHO WshShell.SendKeys "{F11}"                            >>"%TempVBSFile%
+ECHO Wscript.Sleep 900                                    >>"%TempVBSFile%"
+
+CSCRIPT //nologo "%TempVBSFile%"
+
+@echo off
+color %%7
+title RYU
+start:
+for /F "tokens=*delims=" %%A in  ( ryu.TXT) do    ECHO  %%A 
+   
+
+
+ pause >nul
+start cmd.exe
